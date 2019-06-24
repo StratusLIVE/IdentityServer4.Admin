6c349a8ec1f9f1e9666bfb5868e5521b3f1d560b
John Funari <jgfunari@hotmail.com>
2019-06-24 17:12:02 -0400

    Removed seeding by default.

--- src/Skoruba.IdentityServer4.Admin/Program.cs
+++ src/Skoruba.IdentityServer4.Admin/Program.cs
@@ -37,7 +37,7 @@ namespace Skoruba.IdentityServer4.Admin
                 .Build();
 
             // Uncomment this to seed upon startup, alternatively pass in `dotnet run /seed` to seed using CLI
-            await DbMigrationHelpers.EnsureSeedData<IdentityServerConfigurationDbContext, AdminIdentityDbContext, IdentityServerPersistedGrantDbContext, AdminLogDbContext, UserIdentity, UserIdentityRole>(host);
+             //await DbMigrationHelpers.EnsureSeedData<IdentityServerConfigurationDbContext, AdminIdentityDbContext, IdentityServerPersistedGrantDbContext, AdminLogDbContext, UserIdentity, UserIdentityRole>(host);
             if (seed)
             {
                 await DbMigrationHelpers.EnsureSeedData<IdentityServerConfigurationDbContext, AdminIdentityDbContext, IdentityServerPersistedGrantDbContext, AdminLogDbContext, UserIdentity, UserIdentityRole>(host);
